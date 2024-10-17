ec4b2de249a50b4bb63b8bbb4880c50a0018536a
Chona <chonajvr@gmail.com>
2024-10-18 00:14:18 +0200

    Add Radzon Design Components

---
+++ SalesOrderManagement.Client/Components/Pages/Error.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+
+namespace SalesOrderManagement.Client.Components.Pages;
+
+public class ErrorBase : ComponentBase
+{
+    [CascadingParameter]
+    public HttpContext? HttpContext { get; set; }
+
+    public string? RequestId { get; set; }
+
+    protected bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    protected override void OnInitialized()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext?.TraceIdentifier;
+    }
+}
---
+++ SalesOrderManagement.Client/Components/Pages/Home.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SalesOrderManagement.Client.Components.Pages;
+
+public class HomeBase : ComponentBase
+{
+    [Inject] public NavigationManager NavigationManager { get; set; }
+
+    protected void NavigateToOverview()
+    {
+        NavigationManager.NavigateTo("/orders/overview");
+    }
+}
