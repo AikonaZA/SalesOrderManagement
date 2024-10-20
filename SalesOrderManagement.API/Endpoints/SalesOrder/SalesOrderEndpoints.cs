033fde62ea0efcbedb243764229eee0e03135980
Chona <chonajvr@gmail.com>
2024-10-20 21:18:46 +0200

    Update Minimal Endpoints to Typed Results

--- SalesOrderManagement.API/Endpoints/SalesOrder/SalesOrderEndpoints.cs
+++ SalesOrderManagement.API/Endpoints/SalesOrder/SalesOrderEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Carter;
+using Microsoft.AspNetCore.Http.HttpResults;
 using SalesOrderManagement.Application.DTOs.SalesOrder;
 using SalesOrderManagement.Application.Interfaces;
 
@@ -8,7 +9,8 @@ public class SalesOrderEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        var group = app.MapGroup("/api/salesorder");
+        var group = app.MapGroup("/api/salesorder")
+            .WithOpenApi();
 
         group.MapPost("", CreateSalesOrder).WithName(nameof(CreateSalesOrder));
 
@@ -21,56 +23,56 @@ public class SalesOrderEndpoints : ICarterModule
         group.MapDelete("{id}", DeleteSalesOrder).WithName(nameof(DeleteSalesOrder));
     }
 
-    public static async Task<IResult> CreateSalesOrder(ISalesOrderService salesOrderService, SalesOrderRequestDto salesOrderRequestDto)
+    public static async Task<Results<Created<SalesOrderRequestDto>, BadRequest<string>>> CreateSalesOrder(ISalesOrderService salesOrderService, SalesOrderRequestDto salesOrderRequestDto)
     {
         if (salesOrderRequestDto == null)
-            return Results.BadRequest("Sales order data is required.");
+            return TypedResults.BadRequest("Sales order data is required.");
 
         await salesOrderService.CreateSalesOrderAsync(salesOrderRequestDto);
 
-        return Results.Created($"/api/salesorder/{salesOrderRequestDto.SalesOrder.SalesOrderRef}", salesOrderRequestDto);
+        return TypedResults.Created($"/api/salesorder/{salesOrderRequestDto.SalesOrder.SalesOrderRef}", salesOrderRequestDto);
     }
 
-    public static async Task<IResult> GetSalesOrders(ISalesOrderService salesOrderService)
+    public static async Task<Results<Ok<IEnumerable<SalesOrderDto>>,NoContent>> GetSalesOrders(ISalesOrderService salesOrderService)
     {
         var salesOrders = await salesOrderService.GetSalesOrdersAsync();
 
         return salesOrders == null || !salesOrders.Any() 
-            ? Results.NoContent() 
-            : Results.Ok(salesOrders);
+            ? TypedResults.NoContent() 
+            : TypedResults.Ok(salesOrders);
     }
 
-    public static async Task<IResult> GetSalesOrder(ISalesOrderService salesOrderService, int id)
+    public static async Task<Results<Ok<SalesOrderDto>, NotFound<string>>> GetSalesOrder(ISalesOrderService salesOrderService, int id)
     {
         var salesOrder = await salesOrderService.GetSalesOrderByIdAsync(id);
 
         return salesOrder == null
-            ? Results.NotFound($"Sales order with ID {id} not found.")
-            : Results.Ok(salesOrder);
+            ? TypedResults.NotFound($"Sales order with ID {id} not found.")
+            : TypedResults.Ok(salesOrder);
     }
 
-    public static async Task<IResult> UpdateSalesOrder(ISalesOrderService salesOrderService, int id, SalesOrderDto salesOrderDto)
+    public static async Task<Results<NoContent, NotFound<string>, BadRequest<string>>> UpdateSalesOrder(ISalesOrderService salesOrderService, int id, SalesOrderDto salesOrderDto)
     {
         if (id != salesOrderDto.Id)
-            return Results.BadRequest("Sales order ID mismatch.");
+            return TypedResults.BadRequest("Sales order ID mismatch.");
 
         var existingOrder = await salesOrderService.GetSalesOrderByIdAsync(id);
         if (existingOrder == null)
-            return Results.NotFound($"Sales order with ID {id} not found.");
+            return TypedResults.NotFound($"Sales order with ID {id} not found.");
 
         await salesOrderService.UpdateSalesOrderAsync(salesOrderDto);
 
-        return Results.NoContent();
+        return TypedResults.NoContent();
     }
 
-    public static async Task<IResult> DeleteSalesOrder(ISalesOrderService salesOrderService, int id)
+    public static async Task<Results<NoContent,NotFound<string>>> DeleteSalesOrder(ISalesOrderService salesOrderService, int id)
     {
         var salesOrder = await salesOrderService.GetSalesOrderByIdAsync(id);
         if (salesOrder == null)
-            return Results.NotFound($"Sales order with ID {id} not found.");
+            return TypedResults.NotFound($"Sales order with ID {id} not found.");
 
         await salesOrderService.DeleteSalesOrderAsync(id);
 
-        return Results.NoContent();
+        return TypedResults.NoContent();
     }
 }
